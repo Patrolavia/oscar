a47ffa8af50ddbf125e56d66c3834127e7692dcb
Fraina <jscaem@gmail.com>
2015-10-02 12:14:22 +0800

    Editor logic: authority

--- src/js/containers/Editor.js
+++ src/js/containers/Editor.js
@@ -2,7 +2,7 @@ import React, { Component, PropTypes } from 'react';
 import { findDOMNode } from 'react-dom';
 import { connect } from 'react-redux';
 import { fetchPad, fetchUser } from 'actions';
-import { each, findWhere } from 'lodash';
+import { each, findWhere, union, intersection } from 'lodash';
 import classNames from 'classnames';
 
 import EditorTitle from 'components/EditorTitle';
@@ -34,11 +34,15 @@ export default class Editor extends Component {
 
     const { result, data, users } = nextProps;
     const { fetchUser } = this.props;
-    const isStillFetching = (data.cooperator) ? data.cooperator.length !== this.state.cooperator.length : false;
     const cooperatorList = [];
     const fetchQueue = [];
 
-    if (result && isStillFetching || data.cooperator === undefined) {
+    const fetchingCooperator = (data.cooperator) ? data.cooperator.length !== this.state.cooperator.length : false;
+    const isStillFetching = result && fetchingCooperator;
+    const noCooperator = result && data.cooperator.length === 0;
+    const noSuchPad = data.cooperator === undefined;
+
+    if (isStillFetching || noCooperator || noSuchPad) {
       const { cooperator, tags } = data;
       each(cooperator, (value) => {
         const currentUser = findWhere(users.data, { 'id': value });
@@ -63,17 +67,28 @@ export default class Editor extends Component {
 
   render() {
     const { isLogged } = this.state;
-    const { result: fetchResult } = this.props;
-    // TODO: is cooperator or owner
+    const { result: fetchResult, auth, data: { user: ownerId, cooperator } } = this.props;
+
+    const hasAuthorized = () => {
+      let userId;
+      if (auth.result) {
+        userId = auth.data.id;
+      } else {
+        return false;
+      }
+
+      return intersection([userId], union([ownerId], cooperator)).length > 0;
+    }
+
     return (
-      <div className={classNames('editPad', {'is-disable': ! isLogged || ! fetchResult})} ref="editPad">
+      <div className={classNames('editPad', {'is-disable': ! isLogged || ! fetchResult || ! hasAuthorized()})} ref="editPad">
         <div className="editPad-title">
           <span className="editPad-optionTitle">Pad title</span>
-          <EditorTitle ref="EditTitle" { ...this.props } authority={ isLogged && fetchResult }/>
+          <EditorTitle ref="EditTitle" { ...this.props } authority={ isLogged && fetchResult && hasAuthorized() }/>
         </div>
         <div className="editPad-content">
           <span className="editPad-optionTitle">Content</span>
-          <EditorContent ref="EditContent" { ...this.props } authority={ isLogged && fetchResult } />
+          <EditorContent ref="EditContent" { ...this.props } authority={ isLogged && fetchResult && hasAuthorized() } />
         </div>
         <div className="editPad-options">
           <div className="editPad-cooperates">
