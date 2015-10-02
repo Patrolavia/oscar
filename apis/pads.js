f45399d863b1a315d452e61f3d93356de4170599
Fraina <jscaem@gmail.com>
2015-10-02 11:48:40 +0800

    update mock api

--- apis/pads.js
+++ apis/pads.js
@@ -4,24 +4,31 @@ module.exports = {
   'data': [
     {
       'title': 'Pad-1 title',
-      'user': 0,
+      'user': 1,
       'cooperator': [],
       'tags': [],
       'id': 1
     },
     {
       'title': 'Pad-2 title',
-      'user': 1,
+      'user': 2,
       'cooperator': [1],
       'tags': ['Tag'],
       'id': 2
     },
     {
+      'title': 'Pad-3 title',
+      'user': 3,
+      'cooperator': [],
+      'tags': ['No authority'],
+      'id': 3
+    },
+    {
       'title': 'mdpad API',
-      'user': 2,
+      'user': 3,
       'cooperator': [1, 2],
       'tags': ['Tag', 'Another'],
-      'id': 3
+      'id': 4
     }
   ]
 };
